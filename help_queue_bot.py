f11e1b27c6b7930c93c412a5e5074dd2e7573c32
Jonas Camillus Jeppesen <jonascj@jonascj.dk>
2020-03-27 01:09:46 +0100

    Quite a rewrite

--- help_queue_bot.py
+++ help_queue_bot.py
@@ -1,107 +1,312 @@
-import os
 from datetime import datetime
+import json
+import logging
+import os
+import sys
 
+import discord
 from discord.ext import commands
 import dotenv
 
+# Load .evn variables 
 dotenv.load_dotenv()
+
+# Logging
+LOG_LVL = os.getenv('LOG_LVL')
+log = logging.getLogger(__name__)
+if not LOG_LVL:
+    LOG_LVL = 'DEBUG'
+
+h = logging.StreamHandler(sys.stdout)
+h.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+log.addHandler(h)
+log.setLevel(LOG_LVL)
+
+# Bot configuration
 TOKEN = os.getenv('DISCORD_TOKEN')
 ADMIN_USER_IDS = [int(s.strip()) for s in os.getenv('ADMIN_USER_IDS').split(',')]
 ADMIN_ROLE_NAMES = [s.strip() for s in os.getenv('ADMIN_ROLE_NAMES').split(',')]
 
+if not TOKEN:
+    log.error('`DISCORD_TOKEN` must be set in .env file')
+    sys.exit()
+
+if not ADMIN_USER_IDS and not ADMIN_ROLE_NAMES:
+    log.error('Either `ADMIN_USER_IDS`, `ADMIN_ROLE_NAMES` or both '
+              'must be set in .env file')
+    sys.exit()
+
+
+# Create bot
 bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
 
-help_queue = {}
+# Queue file/disk handling
+DISK_FILE = 'help_queue.json'
+try:
+    with open(DISK_FILE, 'r') as f:
+        help_queue = json.load(f)
+        log.info('Loaded existing queue from file/disk')
+except:
+    help_queue = {}
+    log.info('Created new empty queue')
 
-def get_guild_queue(context):
-    key = (context.guild.id, context.guild.name)
+def save_to_disk():
+    with open('help_queue.json', 'w') as f:
+        json.dump(help_queue, f)
+
+
+# Bot helper functions
+def get_guild_queue(ctx):
+    key = '{}#{}'.format(ctx.guild.name,ctx.guild.id)
     if key not in help_queue:
         help_queue[key] = []
     return help_queue[key]
 
-def get_user_name(context):
-    if context.author.nick != '':
-        return context.author.nick
-    else:
-        return context.author.name
 
-def auth_admin(context):
-    if context.author.id in ADMIN_USER_IDS:
+def get_user_name(user):
+    if user.nick:
+        # User has a server/guild nick set, use that
+        return user.nick
+    else:
+        # Otherwise just use the user's account namt
+        return user.name
+
+
+def auth_admin(user):
+    if user.id in ADMIN_USER_IDS:
         return True
-    
-    for r in context.author.roles:
+
+    for r in user.roles:
         if r in ADMIN_ROLE_NAMES:
             return True
 
     return False
-
     
+
+
+
 
 @bot.event
 async def on_ready():
-    print('Bot logged in as {0.user}'.format(bot))
+    log.info('Bot logged in as `%s` (id %s)', bot.user, bot.user.id)
+    log.info('Member of the following servers/guilds:')
+    for guild in bot.guilds:
+        log.info('  %s', guild.name)
 
 
-@bot.group(aliases=['q'], help='Queue management', invoke_without_command=True)
-async def queue(c):
-    await c.send('Please provide a subcommand, type `!help queue` for help')
+
+@bot.command(name='qu', aliases=['needhelp'], help='Queue up for help')
+async def queue_up(ctx, msg=""):
+            
+    queue = get_guild_queue(ctx)
+    for entry in queue:
+        if entry['author_id'] == ctx.author.id:
+            await ctx.send('{} You are already in the queue'.
+                            format(ctx.author.mention))
+            return
+
+    entry = {'name': get_user_name(ctx.author),
+             'author_id': ctx.author.id,
+             'datetime': datetime.strftime(datetime.now(),'%Y-%m-%d %H:%M:%S'),
+             'msg': msg} 
+
+    queue.append(entry)
+
+    await ctx.send('{} You queued up for help'
+                   '(position in queue: {})'
+                   .format(ctx.author.mention, len(queue)))
+
+    # Heads-up to queuing member about being first in line already
+    if len(queue) == 1:
+        try:
+            await ctx.author.send('You\'re next in line, be ready for '
+                                  'help / support / discussion '
+                                  '(consider joining a voice channel!).')
+        except:
+            log.info('Failed to direct message member `%s` heads-up '
+                     'about being first in line upon queuing',
+                     get_user_name(ctx.author))
+    save_to_disk()
+
+
+
+@bot.command(name='qs', aliases=['show'], help='Show the queue')
+async def queue_show(ctx):
+    queue = get_guild_queue(ctx)
+    now = datetime.now()
+    
+    embed = discord.Embed(title='Queue', colour = discord.Colour.blue())
+    if len(queue) == 0:
+        value='The queue is empty!'
+    else:
+        entries = []
+        for entry in queue:
+            joined = datetime.strptime(entry['datetime'], '%Y-%m-%d %H:%M:%S') 
+            wait_min = (now-joined).seconds / 60
+
+            if entry['msg']:
+                entries.append('**{}** *({:.0f} min)*: {}'
+                               .format(entry['name'], wait_min, entry['msg']))
+            else:
+                entries.append('**{}** *({:.0f} min)*'
+                               .format(entry['name'], wait_min))
+        value='\n'.join(entries)
+
+    name = 'Users in the queue ({})'.format(len(queue))
+    embed.add_field(name=name, value=value, inline=False)
+
+    await ctx.send(embed=embed)
+
+
+
+@bot.command(name='ql', aliases=['nvm'], help='Leave the help queue')
+async def queue_leave(ctx):
+    queue = get_guild_queue(ctx)
+    for i, entry in enumerate(queue):
+        if entry['author_id'] == ctx.author.id:
+            del queue[i]
+            await ctx.send('{} You left the queue'
+                           .format(ctx.author.mention))
+            return 
+
+    await ctx.send('{} You are not in the queue and therefor can not leave it'
+                   .format(ctx.author.mention))
+
+    save_to_disk()
+
+
+
+@bot.command(name='qe', help='Empty the queue (admin)')
+async def queue_empty(ctx):
+    if auth_admin(ctx.author):
+        queue = get_guild_queue(ctx)
+        queue.clear()
+        await ctx.send('{} You emptied the queue'.format(ctx.author.mention))
+    else:
+        log.warning('Member `%s` (id %s) not authorized to empty queue', 
+                    get_user_name(ctx.author), ctx.author.id)
+        await ctx.send('{} Permission denied'.format(ctx.author.mention))
+
+    save_to_disk()
     
 
-@queue.group(pass_context=True)
-async def show(c):
-    q = get_guild_queue(c)
-    now = datetime.now()
 
-    output = 'Queue ({})\n'.format(len(q))
-    for entry in q:
-        wait_sec = (now-entry['datetime']).seconds
-        line = '{} ({:.0f} min)'.format(entry['name'], 
-                                          wait_sec/60)
-        output += '    ' + line + '\n'
+@bot.command(name='qn', aliases=['next'], help='Help the next in line (admin)')
+async def queue_next(ctx, message=""):
+    if not auth_admin(ctx.author):
+        log.warning('Member `%s` (id %s) not authorized to dequeue', 
+                    get_user_name(ctx.author), ctx.author.id)
+        await ctx.send('{} Permission denied'.format(ctx.author.mention))
+        return
 
-    await c.send(output)
-
-
-@queue.group(pass_context=True)
-async def empty(c):
-    if auth_admin(c):
-        q = get_guild_queue(c)
-        q.clear()
+   
+    # Dequeue next member (in queue)
+    queue = get_guild_queue(ctx)
+    if len(queue) == 0:
+        await ctx.send('The queue is empty, nobody is requesting help!')
+        return
     else:
-        await c.send('Permission denied')
+        _next = queue.pop(0) # FIFO queue
+        next_member = discord.utils.get(ctx.guild.members,id=_next['author_id'])
 
+    if not next_member:
+        await ctx.send('**{}** is not online or has left the server'
+                       .format(_next['name']))
+        return
+    
+    # Alert the next member
+    try:
+        await next_member.send('It is your turn to receive help/support '
+                               'and/or discuss!')
+    except:
+        log.info('Unable to alert next member `%s`', get_user_name(next_member))
+
+        await ctx.send('{} It is your turn to receive help/support '
+                       'and/or discuss!'
+                       .format(next_member.mention))
+
+
+    # Move member to admin voice channel (if are connected to a voice channel)
+    if ctx.author.voice and next_member.voice:
+        try:
+            await next_member.move_to(ctx.author.voice.channel,
+                            reason='To facilitate help/support/discussion')
+        except:
+            log.info('Failed to move `{}` to voice channel of `{}`'
+                     .format(get_user_name(next_member), get_user_name(ctx.author)))
+
+    else:
+        await ctx.send('{} {} Failed to move you to the same voice channel, '
+                       'you will have to find each other manually'
+                       .format(ctx.author.mention, next_member.mention))
+
+    
+    # Heads-up to (next) next member (now at the front of the queue)
+    if len(queue) == 0:
+        return
         
+    _nextnext = queue[0] # Do not dequeue
+    try:
+        nextnext_member = discord.utils.get(ctx.guild.members,
+                                            id=_nextnext['author_id'])
 
-@queue.group(help='Places you in the help queue')
-async def enter(c):
-    q = get_guild_queue(c)
-    for e in q:
-        if e['author_id'] == c.author.id:
-            await c.send('You are already in the queue {}'.format(get_user_name(c)))
-            return
+        await nextnext_member.send('You\'re next in line, be ready for '
+                                   'help / support / discussion '
+                                   '(consider joining a voice channel!).')
+    except:
+        log.info('Unable to send heads-up to `%s`',_nextnext['name'])
 
-    entry = {'name': get_user_name(c),
-             'author_id': c.author.id,
-             'datetime': datetime.now(),
-             'msg': c.message.content} 
+    
 
-    q.append(entry)
-
-    await c.send('Queued {} for help (queue length: {})'.format(c.author.name, len(q)))
-
-
-@bot.command(help='Places you in the help queue')
-async def needhelp(c):
-    await enter.invoke(c)
+@bot.command(name="devdump", hidden=True)
+async def devdump(ctx):
+    log.info('## Dev dump ##')
+    log.info('ctx.author.nick: %s', ctx.author.nick)
+    log.info('ctx.author.name: %s', ctx.author.name)
+    log.info('str(ctx.author): %s', str(ctx.author))
+    log.info('ctx.author.mention: %s', ctx.author.mention)
 
 
-@queue.group(help='Removes you from the help queue')
-async def leave(c):
-    q = get_guild_queue(c)
-    for e in q:
-        if e['author_id'] == c.author.id:
-            await c.send('You are already in the queue {}'.format(get_user_name(c)))
-            return
+@bot.command(name='help')
+async def help(ctx):
 
+    about_text = ('A simple bot which handles a simple (fifo) queue '
+                 'for help / support / private discussion requests.\n\n'
+                 'One or more admin members can dequeue members from the queue '
+                 'to offer help / support / private discussion '
+                 'in an orderly fashion.')
 
-bot.run(TOKEN)
+    embed = discord.Embed(colour = discord.Colour.blue())
+    embed.set_author(name='Help')
+    embed.add_field(name='About', value=about_text, inline=False)
+                          
+    embed.add_field(name='`!qu [msg]` | `!needhelp [msg]`',
+                    value=('Queues you up for help with an optional message. '
+                           'Quote messages with space, e.g. *!qu "what ever"*.'),
+                    inline=False)
+
+    embed.add_field(name='`!ql` | `!nvm`',
+                    value='Leave the help queue',
+                    inline=False)
+
+    embed.add_field(name='`!qs` | `!show`',
+                    value='Show the queue',
+                    inline=False)
+
+    embed.add_field(name='`!qn`|`!next` (admin)',
+                    value='Help the next in line',
+                    inline=False)
+
+    embed.add_field(name='`!qe` (admin)',
+                    value='Empty the queue for this server/guild',
+                    inline=False)
+
+    await ctx.send(embed=embed)
+
+try:
+    bot.run(TOKEN)
+except discord.errors.LoginFailure:
+    log.error('Bot login failed, improper/invalid token')
+    sys.exit()
+
